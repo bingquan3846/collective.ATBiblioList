bc21d4ac7ce66067a0027dcb89416c05467d5e1d
davconvent <davconvent@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2004-11-18 20:24:04 +0000

    moved the list sorting process to FSPythonScript in skins so that it's easily customizable
    git-svn-id: https://svn.plone.org/svn/collective/trunk@804 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- skins/bibliography_list/sortBibrefDictList.py
+++ skins/bibliography_list/sortBibrefDictList.py
@@ -4,24 +4,24 @@
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=dictlist
+##parameters=dictlist=()
 ##title=sortBibrefDictList
 ##
 
 # sorts a list of bibref dictonnaries using two different methods
-# 1st sorting key: firts author's lastname
+# 1st sorting key: first author's lastname
 # 2nd sorting key: publication_year
-
-def cmpLName(self, dica, dicb):
-    autha, authb = dica.get('authors'), dicb.get('authors')
-    if autha and authb:
-        return cmp(autha[0].get('lastname'), authb[0].get('lastname'))
-    return -1
 
 # method 1: publication_year
 tmplist = [(x['publication_year'],x) for x in dictlist]
 dictlist = [y for (x,y) in tmplist.sort()]
 
 # method 2: authors[0]['lastname']
+def cmpLName(self, dica, dicb):
+    autha, authb = dica.get('authors'), dicb.get('authors')
+    if autha and authb:
+        return cmp(autha[0].get('lastname'), authb[0].get('lastname'))
+    return -1
 dictlist.sort(self.cmpLName)
+
 return dictlist
