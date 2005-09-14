2ecd0faa96141399e32ab44a1831c7e39ad3d592
davconvent <davconvent@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2005-09-14 08:13:06 +0000

    fixed module aliases for styles - this was the last oops
    git-svn-id: https://svn.plone.org/svn/collective/ATBiblioList/trunk@11844 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- modulealiases.py
+++ modulealiases.py
@@ -1,6 +1,14 @@
-import sys, content, tool
+import sys, content, tool, styles
 sys.modules['Products.ATBiblioList.BibliographyList'] = content.bibliolist
 sys.modules['Products.ATBiblioList.BibrefCustomStyle'] = content.customstyle
 sys.modules['Products.ATBiblioList.BibrefCustomStyleFolder'] = content.customstylefolder
 sys.modules['Products.ATBiblioList.BibrefCustomStyleSet'] = content.customstyleset
+
 sys.modules['Products.ATBiblioList.BiblioListTool'] = tool.bibliolist
+
+sys.modules['Products.ATBiblioList.styles.Minimal'] = styles.minimal
+sys.modules['Products.ATBiblioList.styles.APA'] = styles.apa
+sys.modules['Products.ATBiblioList.styles.Chicago'] = styles.chicago
+sys.modules['Products.ATBiblioList.styles.Harvard'] = styles.harvard
+sys.modules['Products.ATBiblioList.styles.MLA'] = styles.mla
+
